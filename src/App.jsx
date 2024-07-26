74d2c8d21d03aee67fbd8a472a0822392b89f359
ifedayo <ifedayyo12@gmail.com>
2024-07-26 16:02:05 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
-
 
 export default function App() {
   //define our routes in jsx
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="product" element={<Product/>}
+        <Route path="product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
