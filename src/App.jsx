2236ed4864e64d2cef6e032b840e7ed8e8e5b10b
ifedayo <ifedayyo12@gmail.com>
2024-07-26 16:01:37 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+
+
 export default function App() {
-  return <div>world</div>;
+  //define our routes in jsx
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product/>}
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+//define our routes in jsx
