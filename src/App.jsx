8b502f89dcf55a5511fbe7899160beb84f3ebb76
ifedayo <ifedayyo12@gmail.com>
2024-07-30 16:11:13 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   //define our routes in jsx
@@ -13,7 +12,7 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/" element={<Homepage />} />
-        <Route path="app" element={<AppLayout />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
