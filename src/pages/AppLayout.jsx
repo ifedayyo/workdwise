b9d2bfe2209a6da3ece3daadbd6e584927fa96ab
ifedayo <ifedayyo12@gmail.com>
2024-08-05 14:14:25 +0100

    push

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,7 +1,8 @@
 import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 export default function AppLayout() {
   return (
-    <div>
+    <div className={styles.app}>
       <Sidebar />
     </div>
   );
