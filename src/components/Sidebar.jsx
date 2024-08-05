664bcc8ba4c19e1d273a991797b3531f14566d26
ifedayo <ifedayyo12@gmail.com>
2024-08-05 14:07:57 +0100

    push

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
 import AppNav from "./AppNav";
+
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,4 +1,3 @@
-import AppNav from "../components/AppNav";
 import Sidebar from "../components/Sidebar";
 export default function AppLayout() {
   return (
