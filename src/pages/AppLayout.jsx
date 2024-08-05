d36c19bf74d625407025e0c051d73e6fef867d25
ifedayo <ifedayyo12@gmail.com>
2024-08-05 13:35:41 +0100

    push

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,9 @@
+import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+export default function AppLayout() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
