000ca9c6cf948a81a03e87106aef7051ad9b6390
ifedayo <ifedayyo12@gmail.com>
2024-08-06 16:00:49 +0100

    push

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,14 @@
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 export default function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
-  return <ul className={styles.cityList}>
-    {cities.map(city =>)}
-  </ul>;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem />
+      ))}
+    </ul>
+  );
 }
