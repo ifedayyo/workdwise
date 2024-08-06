ddc3adbbbb8de0ae4dd42a6245102fd84ad2f574
ifedayo <ifedayyo12@gmail.com>
2024-08-06 15:52:56 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,28 @@ import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
+import { useState, useEffect } from "react";
 
+const BASE_URL = "https://localhost:8000";
 export default function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   //define our routes in jsx
   return (
     <BrowserRouter>
@@ -17,7 +37,10 @@ export default function App() {
         <Route index element={<Homepage />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
