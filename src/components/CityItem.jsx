bd1878370b4ab57731636c789c82e08df4d9c8d7
ifedayo <ifedayyo12@gmail.com>
2024-08-06 16:24:31 +0100

    push

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,9 +1,12 @@
 import styles from "./CityItem.module.css";
 
 export default function CityItem({ city }) {
+  const { cityName, emoji, date } = city;
   return (
     <li className={styles.CityItem}>
-      <span className={styles.emoji}></span>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{date}</time>
     </li>
   );
 }
