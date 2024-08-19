6d61033d20836940b68a8e0943a531b540fda462
ifedayo <ifedayyo12@gmail.com>
2024-08-19 12:02:52 +0100

    oush

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -9,10 +9,10 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
   return (
-    <li className={styles.CityItem}>
-      <Link>
+    <li>
+      <Link className={styles.CityItem} to={`${id}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
