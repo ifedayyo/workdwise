3cb6ee3ec1ed44e89073720f14e6ed1e9c8563b5
ifedayo <ifedayyo12@gmail.com>
2024-08-23 16:00:55 +0100

    push please

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,9 @@
-export default function Button() {
-  return <div></div>;
+import styles from "./Button.module.css";
+
+export default function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={styles.btn}>
+      {children}
+    </button>
+  );
 }
